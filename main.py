1ed975281525edf6ccab06c0a4c5e4be20056423
5etrovich <petr.bychuk@gmail.com>
2024-01-18 18:29:21 +0300

    add login to the SdamGIA

---
+++ main.py
@@ -0,0 +1,28 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import yaml
+
+
+def login(dr, url, usernameId, username, passwordId, password, submit_buttonId):
+    dr.get(url)
+    sleep(5)
+    dr.find_element(by=By.ID, value=usernameId).send_keys(username)
+    dr.find_element(by=By.ID, value=passwordId).send_keys(password)
+    dr.find_element(by=By.CLASS_NAME, value=submit_buttonId).click()
+
+
+with open('loginDetails.yml', 'rb') as f:
+    conf = yaml.load(f, Loader=yaml.SafeLoader)
+
+myFbEmail = conf['fb_user']['email']
+myFbPassword = conf['fb_user']['password']
+
+service = Service(r"./chromedriver.exe")
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
+
+login(driver, "https://ege.sdamgia.ru/", "email", myFbEmail, "current-password", myFbPassword,
+      "Button.Button_view_default.ProfileAuth-Button")
+sleep(30)
