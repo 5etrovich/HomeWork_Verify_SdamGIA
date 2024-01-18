861680d33b2546d7e504eba383f0289cb77d18a1
5etrovich <petr.bychuk@gmail.com>
2024-01-18 18:48:37 +0300

    add open_test_group

--- main.py
+++ main.py
@@ -13,16 +13,25 @@ def login(dr, url, usernameId, username, passwordId, password, submit_buttonId):
     dr.find_element(by=By.CLASS_NAME, value=submit_buttonId).click()
 
 
+def open_test_group(dr, url, informatica, group):
+    dr.get(url + "/teacher/journal")
+    sleep(10)
+    dr.find_element(by=By.CLASS_NAME, value=informatica).click()
+    dr.find_element(by=By.CLASS_NAME, value=group).click()
+
+
 with open('loginDetails.yml', 'rb') as f:
     conf = yaml.load(f, Loader=yaml.SafeLoader)
 
 myFbEmail = conf['fb_user']['email']
 myFbPassword = conf['fb_user']['password']
-
+main_url = "https://ege.sdamgia.ru"
 service = Service(r"./chromedriver.exe")
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 
-login(driver, "https://ege.sdamgia.ru/", "email", myFbEmail, "current-password", myFbPassword,
+login(driver, main_url, "email", myFbEmail, "current-password", myFbPassword,
       "Button.Button_view_default.ProfileAuth-Button")
-sleep(30)
+sleep(5)
+open_test_group(driver, main_url, "PortalList-SubjectName", "PortalList-Group")
+
